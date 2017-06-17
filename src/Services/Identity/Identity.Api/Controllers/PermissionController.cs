bebc392b7b5f548251af8d73c900a0bf93e8d49c
wei <wei@DESKTOP-DHCJOJ9>
2017-06-17 16:46:10 +0800

    remove  internal  authorize

--- src/Services/Identity/Identity.Api/Controllers/PermissionController.cs
+++ src/Services/Identity/Identity.Api/Controllers/PermissionController.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace Rwby.Identity.Controllers
 
 {
-    [Authorize]
+    //[Authorize]
     [Route("[controller]")]
     public class PermissionController : Controller
     {
